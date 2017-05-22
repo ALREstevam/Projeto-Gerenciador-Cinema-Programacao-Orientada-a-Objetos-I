294e1fc317450f407290db956c19e4ad73ba374d
ALREstevam <a166348@g.unicamp.com>
2017-05-22 10:46:14 -0300

    adicionando bd e bibliotecas

--- Cinema/main.cpp
+++ Cinema/main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <stdio.h>
+#include "sqlite3/sqlite3.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+
+   sqlite3 *db;
+   char *zErrMsg = 0;
+   int rc;
+
+   rc = sqlite3_open("db.db", &db);
+
+   if(rc){
+      cout << "Can't open database: %s\n" << sqlite3_errmsg(db) << endl;
+      return(0);
+   }else{
+      cout << "Opened database successfully\n" << endl;
+   }
+   sqlite3_close(db);
+
     return 0;
 }
