3cee887fc21e158d22ee46d64c3f6750d49f2707
pedroArtico <pedro_artico@hotmail.com>
2017-05-24 23:30:01 -0300

    Inserindo classes Data e Filme
    A completar por cont do Banco de Dados

---
+++ Classes Data e Filme/Dataa.h
@@ -0,0 +1,30 @@
+///////////.h////////////////
+#ifndef DATA_H
+#define DATAA_H
+#include <iostream>
+using namespace std;
+class Dataa
+{
+	public:
+		Dataa();
+		Dataa(int diaSemana,int mes, int ano, int hora, int minuto);
+		void setDiaSemana(int diaSemana);
+		int getDiaSemana();
+		void setMes(int mes);
+		int getMes();
+		void setAno(int ano);
+		int getAno();
+		void setHora(int hora);
+		int getHora();
+		void setMinuto(int minuto);
+		int getMinuto();
+	private:
+		int diaSemana;
+		int mes;
+		int ano;
+		int hora;
+		int minuto;
+};
+
+#endif
+
---
+++ Classes Data e Filme/Untitled1.cpp
@@ -0,0 +1,16 @@
+/////////////main//////
+//#include "Dataa.h"
+#include "Filme.h"
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main(){
+	Dataa d1;
+	Filme movie;
+	int d,m,a;
+	char c,c1;
+	movie.cadastrar();
+	
+	return 0;
+}
