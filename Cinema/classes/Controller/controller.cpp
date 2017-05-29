3417df727d2ed4e46934136238b064f68e7fe8c8
ALREstevam <a166348@g.unicamp.com>
2017-05-29 10:01:38 -0300

    DataFilme

---
+++ Cinema/classes/Controller/controller.cpp
@@ -0,0 +1,21 @@
+#ifndef CONTROLLER_CPP
+#define CONTROLLER_CPP
+#include "../Data/Dataa.h"
+#include "../Filme/Filme.h"
+#include <list>
+
+
+class ControllerCinema
+{
+     public:
+          addFilme(Filme fm){
+
+          }
+     private:
+          list<Filme> filmes;
+
+
+
+};
+
+#endif
